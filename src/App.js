7f2716d390141dd054bcb071b210f2130a07222b
ramichabchoub <ramichabchoub.rc@gmail.com>
2022-08-11 11:03:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Container } from "react-bootstrap";
+import React, { useState, useEffect } from 'react'
+import { person } from './data'
+import DatesCount from './components/DatesCount'
+import DatesList from './components/DatesList'
+import DatesAction from './components/DatesAction'
+function App() {
+  
+  const [personData, setPersonData] = useState(person)
+  const onDelete = () => {
+    setPersonData([])
+  }
+  const onViewData = () => {
+    setPersonData(person)
+  }
+  useEffect(() => {
+    setPersonData([])
+  }, [])
+  return (
+    <div className="font color-body">
+      <Container className="py-5">
+        <DatesCount person={personData} />
+        <DatesList person={personData} />
+        <DatesAction deleteData={onDelete} onViewData={onViewData} />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
